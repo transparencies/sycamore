e56aa9ce0b3cba0f7cb316d68e715b1c672f241f
github-action-benchmark <github@users.noreply.github.com>
2024-03-08 18:25:33 +0000

    add Benchmark (cargo) benchmark result for 34d582ba5aa7d54f839d051dc417b16f9c14da9c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709601503660,
+  "lastUpdate": 1709922333349,
   "repoUrl": "https://github.com/transparencies/sycamore",
   "entries": {
     "Benchmark": [
@@ -9803,6 +9803,78 @@ window.BENCHMARK_DATA = {
             "name": "ssr_medium",
             "value": 23719,
             "range": "± 118",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "34ttrweoewiwe28@gmail.com",
+            "name": "mikihiro",
+            "username": "Hmikihiro"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "34d582ba5aa7d54f839d051dc417b16f9c14da9c",
+          "message": "Add current page highlight in website navigation (#660)\n\n* Add underline and bold to navigation\r\n\r\n* cargo fix & cargo clippy\r\n\r\n* remove <Option>\r\n\r\n* rename to SidebarCurrent\r\n\r\n* remove unused_dependencies gloo\r\n\r\n* cargo fix\r\n\r\n* Update website/src/header.rs\r\n\r\nCo-authored-by: Luke <37006668+lukechu10@users.noreply.github.com>\r\n\r\n* Update website/src/main.rs\r\n\r\nCo-authored-by: Luke <37006668+lukechu10@users.noreply.github.com>\r\n\r\n* Update website/src/main.rs\r\n\r\nCo-authored-by: Luke <37006668+lukechu10@users.noreply.github.com>\r\n\r\n* Update website/src/main.rs\r\n\r\nCo-authored-by: Luke <37006668+lukechu10@users.noreply.github.com>\r\n\r\n* Update website/src/main.rs\r\n\r\nCo-authored-by: Luke <37006668+lukechu10@users.noreply.github.com>\r\n\r\n* Update website/src/main.rs\r\n\r\nCo-authored-by: Luke <37006668+lukechu10@users.noreply.github.com>\r\n\r\n* Update website/src/main.rs\r\n\r\nCo-authored-by: Luke <37006668+lukechu10@users.noreply.github.com>\r\n\r\n* Update website/src/sidebar.rs\r\n\r\nCo-authored-by: Luke <37006668+lukechu10@users.noreply.github.com>\r\n\r\n---------\r\n\r\nCo-authored-by: Luke <37006668+lukechu10@users.noreply.github.com>",
+          "timestamp": "2024-03-08T08:52:17Z",
+          "tree_id": "35a97d397e179ad880726c06ec99c0dc4052d522",
+          "url": "https://github.com/transparencies/sycamore/commit/34d582ba5aa7d54f839d051dc417b16f9c14da9c"
+        },
+        "date": 1709922332859,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "reactivity_signals",
+            "value": 33359,
+            "range": "± 590",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "reactivity_effects",
+            "value": 108969,
+            "range": "± 1272",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "reactivity_map_indexed",
+            "value": 12902,
+            "range": "± 677",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "reactivity_map_keyed",
+            "value": 19296,
+            "range": "± 30907",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "reactivity_context_deeply_nested",
+            "value": 1049,
+            "range": "± 203",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "deep_creation",
+            "value": 216969,
+            "range": "± 100181",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ssr_small",
+            "value": 698,
+            "range": "± 25",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ssr_medium",
+            "value": 23995,
+            "range": "± 66",
             "unit": "ns/iter"
           }
         ]
